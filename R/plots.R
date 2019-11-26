f9d33eebfaf43319ff76b0b7a0b85d00df56b446
swd-turner <sean.turner@pnnl.gov>
2019-11-25 18:02:32 -0800

    license etc

---
+++ R/plots.R
@@ -0,0 +1,8 @@
+#' plot_watershed
+#' @details plot watershed for a named reservoir or set of reservoirs
+#' @param reservoir name of reservoir (or reservoirs as vector)
+#' @export
+#'
+plot_watershed <- function(reservoir) {
+  message(paste("plotting watershed for", reservoir))
+}
